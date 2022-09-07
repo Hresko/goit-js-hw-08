8ba4597b9e5b158144652364824069409a77a28b
Hreskoo <hresko21@gmail.com>
2022-09-07 09:51:30 +0300

    home-work-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feadback-form');
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+populateImput();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  e.preventDefault();
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage)
+
+  if (parseMessage) {
+    console.log(parseMessage);
+  }
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateImput() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parseMessage = JSON.parse(savedMessage);
+
+  if (parseMessage) {
+    formData = parseMessage;
+    form.email.value = formData.email || '';
+    form.message.value = formData.message || '';
+  }
+}
